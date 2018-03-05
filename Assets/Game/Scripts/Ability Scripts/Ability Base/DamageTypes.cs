555456e460c256d98b63763f845ac94252bcdfc9
Borgeshc <Borgeshc@gmatc.matc.edu>
2018-03-05 09:08:00 -0600

    Added a base for damagetypes

---
+++ Assets/Game/Scripts/Ability Scripts/Ability Base/DamageTypes.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageTypes : MonoBehaviour
+{
+    public int damage;
+    protected List<ModuleAbility> activeModules = new List<ModuleAbility>();
+
+    public void SetModule(ModuleAbility module)
+    {
+        activeModules.Add(module);
+    }
+
+    public void ApplyModules(GameObject other)
+    {
+        if (activeModules.Count > 0)
+        {
+            for (int i = 0; i < activeModules.Count; i++)
+            {
+                switch (activeModules[i].module)
+                {
+                    case ModuleAbility.Module.Concussion:
+                        other.GetComponent<Modules>().Concussion(activeModules[i].effectLength);
+                        break;
+                    case ModuleAbility.Module.Incinerating:
+                        other.GetComponent<Modules>().Incinerating(activeModules[i].damage, activeModules[i].effectLength);
+                        break;
+                    case ModuleAbility.Module.Crippling:
+                        other.GetComponent<Modules>().Crippling(activeModules[i].effectLength, activeModules[i].slowAmount, activeModules[i].stackAmount);
+                        break;
+                    case ModuleAbility.Module.Weighted:
+                        other.GetComponent<Modules>().Weighted(activeModules[i].pushForce);
+                        break;
+                    case ModuleAbility.Module.Siphon:
+                        float leechDamage = damage
+                            / activeModules[i].leechPercentage;
+                        other.GetComponent<Modules>().Siphon(Mathf.RoundToInt(leechDamage));
+                        break;
+                    case ModuleAbility.Module.Hemorrhage:
+                        other.GetComponent<Modules>().Hemorrhage(activeModules[i].damage, activeModules[i].effectLength, activeModules[i].stackAmount);
+                        break;
+                }
+            }
+        }
+    }
+}
--- Assets/Game/Scripts/Ability Scripts/Ability Base/Trap.cs
+++ Assets/Game/Scripts/Ability Scripts/Ability Base/Trap.cs
@@ -2,17 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Trap : MonoBehaviour
+public class Trap : DamageTypes
 {
-    public int damage;
     public bool destroyedOnTrigger;
-
-    List<ModuleAbility> activeModules = new List<ModuleAbility>();
-
-    public void SetModule(ModuleAbility module)
-    {
-        activeModules.Add(module);
-    }
 
     public virtual void EffectOnTrigger(GameObject objectHit)
     {
@@ -29,41 +21,9 @@ public class Trap : MonoBehaviour
             ApplyModules(other.gameObject);
             //other.GetComponent<Health>().TookDamage(damage);
             EffectOnTrigger(other.gameObject);
+
             if (destroyedOnTrigger)
                 Destroy(gameObject);
-        }
-    }
-
-    void ApplyModules(GameObject other)
-    {
-        if (activeModules.Count > 0)
-        {
-            for (int i = 0; i < activeModules.Count; i++)
-            {
-                switch (activeModules[i].module)
-                {
-                    case ModuleAbility.Module.Concussion:
-                        other.GetComponent<Modules>().Concussion(activeModules[i].effectLength);
-                        break;
-                    case ModuleAbility.Module.Incinerating:
-                        other.GetComponent<Modules>().Incinerating(activeModules[i].damage, activeModules[i].effectLength);
-                        break;
-                    case ModuleAbility.Module.Crippling:
-                        other.GetComponent<Modules>().Crippling(activeModules[i].effectLength, activeModules[i].slowAmount, activeModules[i].stackAmount);
-                        break;
-                    case ModuleAbility.Module.Weighted:
-                        other.GetComponent<Modules>().Weighted(activeModules[i].pushForce);
-                        break;
-                    case ModuleAbility.Module.Siphon:
-                        float leechDamage = damage
-                            / activeModules[i].leechPercentage;
-                        other.GetComponent<Modules>().Siphon(Mathf.RoundToInt(leechDamage));
-                        break;
-                    case ModuleAbility.Module.Hemorrhage:
-                        other.GetComponent<Modules>().Hemorrhage(activeModules[i].damage, activeModules[i].effectLength, activeModules[i].stackAmount);
-                        break;
-                }
-            }
         }
     }
 }
