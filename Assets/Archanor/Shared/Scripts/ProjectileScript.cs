86524929483f6182e3a014bc49d05f807c41915d
Borgeshc <Borgeshc@gmatc.matc.edu>
2018-04-09 09:42:27 -0500

    Added new ground, fixed projectile colliders

--- Assets/Archanor/Shared/Scripts/ProjectileScript.cs
+++ Assets/Archanor/Shared/Scripts/ProjectileScript.cs
@@ -26,30 +26,33 @@ public class ProjectileScript : MonoBehaviour
 
     void OnTriggerEnter(Collider hit)
     {
-        if (!hasCollided)
+        if(hit.tag.Equals("Enemy") || hit.tag.Equals("Ground") || hit.tag.Equals("Environment"))
         {
-            hasCollided = true;
-            //transform.DetachChildren();
-            impactParticle = Instantiate(impactParticle, transform.position, Quaternion.FromToRotation(Vector3.up, impactNormal)) as GameObject;
-            //Debug.DrawRay(hit.contacts[0].point, hit.contacts[0].normal * 1, Color.yellow);
-
-            if (hit.gameObject.tag == "Destructible") // Projectile will destroy objects tagged as Destructible
+            if (!hasCollided)
             {
-                Destroy(hit.gameObject);
-            }
+                hasCollided = true;
+                //transform.DetachChildren();
+                impactParticle = Instantiate(impactParticle, transform.position, Quaternion.FromToRotation(Vector3.up, impactNormal)) as GameObject;
+                //Debug.DrawRay(hit.contacts[0].point, hit.contacts[0].normal * 1, Color.yellow);
+
+                if (hit.gameObject.tag == "Destructible") // Projectile will destroy objects tagged as Destructible
+                {
+                    Destroy(hit.gameObject);
+                }
 
 
-            //yield WaitForSeconds (0.05);
-            foreach (GameObject trail in trailParticles)
-            {
-                GameObject curTrail = transform.Find(projectileParticle.name + "/" + trail.name).gameObject;
-                curTrail.transform.parent = null;
-                Destroy(curTrail, 3f);
+                //yield WaitForSeconds (0.05);
+                foreach (GameObject trail in trailParticles)
+                {
+                    GameObject curTrail = transform.Find(projectileParticle.name + "/" + trail.name).gameObject;
+                    curTrail.transform.parent = null;
+                    Destroy(curTrail, 3f);
+                }
+                Destroy(projectileParticle, 3f);
+                Destroy(impactParticle, 5f);
+                Destroy(gameObject);
+                //projectileParticle.Stop();
             }
-            Destroy(projectileParticle, 3f);
-            Destroy(impactParticle, 5f);
-            Destroy(gameObject);
-            //projectileParticle.Stop();
         }
     }
 }
