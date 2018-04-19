4c53bca2104ff24918f84cc44156f56d3116e82a
Borgeshc <Borgeshc@gmatc.matc.edu>
2018-04-19 12:40:03 -0500

    Added flip animation

--- Assets/Game/Scripts/UI/AbilityCard.cs
+++ Assets/Game/Scripts/UI/AbilityCard.cs
@@ -3,18 +3,62 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.EventSystems;
 
-public class AbilityCard : MonoBehaviour {
-
+public class AbilityCard : Selectable
+{
 	public Ability cardAbility;
 	public Button cardButton;
 	public Image abilityIcon;
 	public TextMeshProUGUI abilityDescription;
+    public Animator anim;
+
+    GraphicRaycaster raycaster;
+    PointerEventData pointerEventData;
+    EventSystem eventSystem;
+    
+    bool flipping;
 
 	public void InitializeCard(Ability _cardAbility)
 	{
 		cardAbility = _cardAbility;
 		abilityIcon.sprite = cardAbility.abilityIcon;
 		abilityDescription.text = cardAbility.abilityDescription;
+
+        eventSystem = GameObject.Find("EventSystem").GetComponent<EventSystem>();
+        raycaster = GameObject.Find("Canvas").GetComponent<GraphicRaycaster>();
 	}
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse1))
+        {
+
+            pointerEventData = new PointerEventData(eventSystem);
+            pointerEventData.position = Input.mousePosition;
+
+            List<RaycastResult> results = new List<RaycastResult>();
+            raycaster.Raycast(pointerEventData, results);
+
+            foreach(RaycastResult result in results)
+            {
+                if(result.gameObject == gameObject)
+                {
+                    if (!flipping)
+                    {
+                        print("Flip");
+                        flipping = true;
+                        anim.SetTrigger("Flip");
+                        StartCoroutine(WaitForFlip());
+                    }
+                }
+            }
+        }
+    }
+
+    IEnumerator WaitForFlip()
+    {
+        yield return new WaitForSeconds(.5f);
+        flipping = false;
+    }
 }
