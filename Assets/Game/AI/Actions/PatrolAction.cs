67fbf6f0396389cc975072ba49dfaa11e4291dde
hectorborges <hectorborges94@gmail.com>
2018-05-14 12:46:03 -0500

    added a patrol enemy for the shooting range

--- Assets/Game/AI/Actions/PatrolAction.cs
+++ Assets/Game/AI/Actions/PatrolAction.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 
 [CreateAssetMenu (menuName = "AI/Actions/Patrol")]
 public class PatrolAction : Action
-{               
+{
+    Patrol patrol;
     public override void Act(StateController controller)
     {
+        if(!patrol)
+        patrol = GameObject.Find("GameManager").GetComponent<Patrol>();
         Patrol(controller);
     }
 
     private void Patrol(StateController controller)
     {
-        //controller.enemyRefManager.navMeshAgent.destination = controller.enemyRefManager.ai.patrolPoints[controller.enemyRefManager.ai.nextWayPoint].position;                      //Set the agent's next waypoint
-        //controller.enemyRefManager.navMeshAgent.isStopped = false;
-        
-        //if(controller.enemyRefManager.navMeshAgent.remainingDistance <= controller.enemyRefManager.navMeshAgent.stoppingDistance && !controller.enemyRefManager.navMeshAgent.pathPending)   //Are we at our destination?
-        //{
-        //    controller.enemyRefManager.ai.nextWayPoint = (controller.enemyRefManager.ai.nextWayPoint + 1) % controller.enemyRefManager.ai.patrolPoints.Length;                  //Increament nextWayPoint or reset it to 0
-        //}
+        controller.enemyRefManager.ai.agent.destination = patrol.patrolPoints[controller.enemyRefManager.ai.nextWayPoint].position;                      //Set the agent's next waypoint
+        controller.enemyRefManager.ai.agent.isStopped = false;
+
+        if (controller.enemyRefManager.ai.agent.remainingDistance <= controller.enemyRefManager.ai.agent.stoppingDistance && !controller.enemyRefManager.ai.agent.pathPending)   //Are we at our destination?
+        {
+            controller.enemyRefManager.ai.nextWayPoint = (controller.enemyRefManager.ai.nextWayPoint + 1) % patrol.patrolPoints.Length;                  //Increament nextWayPoint or reset it to 0
+        }
     }
 }
