6f81459fb68ca83e49405145cbf764c8140b38f0
hectorborges <developer@hectorborges.com>
2018-06-06 08:22:44 -0500

    cleaned folders, added scene reloading at the end

---
+++ Assets/Game/Scripts/Utility/ReloadLevel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReloadLevel : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
